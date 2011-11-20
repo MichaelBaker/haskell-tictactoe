8dd2de7dc24315737e48bf0f1f9c4ab0767e1e0a
Michael Baker <michaeltbaker@gmail.com>
2011-11-20 05:00:28 +0000

    Json request returns a json list

--- Handler/Move.hs
+++ Handler/Move.hs
@@ -2,7 +2,22 @@ module Handler.Move where
 
 import Import
 import Yesod.Json
+import Data.Text hiding (zip, foldr, head, map)
 
-getMoveR :: String -> Handler RepJson
-getMoveR message = do
-  jsonToRepJson $ jsonMap [("message", jsonScalar $ message ++ " in bed")]
+getMoveR :: Handler RepJson
+getMoveR = do
+  board <- lookupGetParam boardParam
+  move  <- lookupGetParam moveParam
+
+  jsonToRepJson $ jsonMap [("newBoard", jsonList $ newBoard board)]
+
+  where newBoard board = case board of
+                           Nothing      -> [jsonScalar "Nothing"]
+                           Just message -> map (jsonScalar . (:[])) $ foldr (\a b -> (head $ show $ snd a):(fst a):b ) "" result
+                             where result = zip (unpack message) [0..8] :: [(Char, Integer)]
+
+boardParam :: Text
+boardParam = pack "board"
+
+moveParam :: Text
+moveParam = pack "move"
